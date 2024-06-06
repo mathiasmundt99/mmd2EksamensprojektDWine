6e7bd227454b23521af32e7e4f6f99a1bfb3acfe
Amalieandreasen <148330357+Amalieandreasen@users.noreply.github.com>
2024-06-06 12:33:18 +0200

    opstart på events, javascript er sat ind

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -9,22 +9,25 @@ burgerMenu.addEventListener("click", ()=>{
    navMenu.classList.toggle("active")
 })
 
+
 //her fanges alle links i navigationen, og når der trykkes på et link så fjerner den klassen .active. Denne funktion gør at navigation lukker når der trykkes på et link.
 document.querySelectorAll(".navLink").forEach(closenav => closenav.addEventListener("click", ()=>{
     burgerMenu.classList.remove("active")
    navMenu.classList.remove("active")
 }))
 
-//Javascript til at fange vores data fra headless cms
+//variabler hvor vi gemmer baseurl og sektioner hvor den hentede data senere skal placeres i
 const baseURL = 'https://api.mastth.dk/wp-json/wp/v2';
 const sectionVin = document.querySelector(".månedensVine");
+const sectionEvents = document.querySelector(".eventsCTA")
 
+//Javascript til at fange vores data fra wordpress apiet til månedens vine
     fetch(baseURL+`/posts?categories=13&per_page=20`)
     .then(res => res.json())
     .then(data=> renderWinePosts(data))  
     .catch(err => console.log("Error: ",err))
 
-
+//funktion til at vise dataen til månedens vine som vi har hentet fra apiet
 function renderWinePosts(winePosts){
 winePosts.forEach(winePost =>{
     const articleEl = document.createElement("article")
@@ -34,18 +37,47 @@ winePosts.forEach(winePost =>{
     const image = document.createElement("img")
     const description = document.createElement("p")
     const price = document.createElement("p")
+    const buttonMV = document.createElement("button")
 
     title.textContent = winePost.acf.title
     label.src = winePost.acf.origin
     image.src = winePost.acf.image.sizes.large
     description.textContent = winePost.acf.description
     price.textContent = "Pris pr. glas:"+ winePost.acf.price
+    buttonMV.textContent = "Læs mere om vinen"
 
-    articleEl.append(title,label,image,description,price)
+    articleEl.append(title,label,image,description,price, buttonMV)
 
 })
 }
 
+
+//Javascript til at fange vores data fra wordpress apiet til vinsmagning og events
+    fetch(baseURL+`/posts?categories=14&per_page=20`)
+    .then(res => res.json())
+    .then(data=> renderEventPosts(data))  
+    .catch(err => console.log("Error: ",err))
+
+ //funktion til at vise dataen til vinsmagning og events som vi har hentet fra apiet
+function renderEventPosts(eventPosts){
+    eventPosts.forEach(eventPost =>{
+        const articleEvents = document.createElement("article")
+        sectionEvents.append(articleEvents)
+        const imageEvents = document.createElement("img")
+        const titleEvents = document.createElement("h3")
+        const date = document.createElement("p")
+        const snackDescription = document.createElement("p")
+        const buttonEventCta = document.createElement("button")
+    
+        imageEvents.src = eventPost.acf.image.sizes.medium
+        titleEvents.textContent = eventPost.acf.title
+        date.textContent = eventPost.acf.location
+        snackDescription.textContent = eventPost.acf.snack_description
+        buttonEventCta.textContent = "Læs mere om eventet"
+    
+        articleEvents.append(imageEvents, titleEvents, date, snackDescription, buttonEventCta)
+    })
+}
 
 
 

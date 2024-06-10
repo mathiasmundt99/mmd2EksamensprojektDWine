f41b6f55454c7abd348ff0da41995cf3869ae45d
mathiasmundt99 <mathiasbeck99@gmail.com>
2024-06-10 11:24:34 +0200

    Slideshow updatet til desktop. 3 img

--- Assets/JS/festHosDwine.js
+++ Assets/JS/festHosDwine.js
@@ -1,30 +1,74 @@
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
 
 
 // Kilde https://www.w3schools.com/howto/howto_js_slideshow.asp
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("slidesImg");
+//     let dots = document.getElementsByClassName("dot");
+//     //Her ittereres igennem alle slides, og der sættes display= none på slides
+//     for (i = 0; i < slides.length; i++) {
+//       slides[i].style.display = "none";  
+//     }
+//     // Her kører et loop som kontrolere om slideIndex er større end slide.lenght
+//     // Hvis/Når den bliver større end .lentgh sættes slideIndex tilbage til 1
+//     // Hvilket giver et loop hvor den køre hele tiden
+//     slideIndex++;
+//     if (slideIndex > slides.length) {slideIndex = 1}    
+//     // Her ittereres igennem alle dots og fjerner "active"
+//     for (i = 0; i < dots.length; i++) {
+//       dots[i].className = dots[i].className.replace(" active", "");
+//     }
+
+
+    
+//     // Her sættes slide til display = block, baseret på slideIndex
+//     slides[slideIndex-1].style.display = "block";  
+//     // Her sættes dots igen til active baseret på slideIndex
+//     dots[slideIndex-1].className += " active";
+//     setTimeout(showSlides, 3500); 
+//   }
+
+let slideIndex = 0;
+showSlides();
+
 function showSlides() {
     let i;
     let slides = document.getElementsByClassName("slidesImg");
     let dots = document.getElementsByClassName("dot");
-    //Her ittereres igennem alle slides, og der sættes display= none på slides
+
+    //Her ittereres igennem alle slides, og der sættes display = none på slides
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+        slides[i].style.display = "none";  
     }
     // Her kører et loop som kontrolere om slideIndex er større end slide.lenght
     // Hvis/Når den bliver større end .lentgh sættes slideIndex tilbage til 1
     // Hvilket giver et loop hvor den køre hele tiden
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1}    
+
     // Her ittereres igennem alle dots og fjerner "active"
     for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    
-    // Her sættes slide til display = block, baseret på slideIndex
-    slides[slideIndex-1].style.display = "block";  
-    // Her sættes dots igen til active baseret på slideIndex
+
+    // Her kontrolleres skærmstørrelsen, og hvis den er 1024 eller over
+    // så skal der istedet vises 3 slides, ellers sættes maxSlides til 1
+    let maxSlides = (window.innerWidth >= 1024) ? 3 : 1;
+    for (i = 0; i < maxSlides; i++) {
+        if (slides[slideIndex + i - 1]) {
+            slides[slideIndex + i - 1].style.display = "block";
+        }
+    }
+
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3500); 
-  }
+    setTimeout(showSlides, 3500);
+}
+
+// Denne funktion er til at kunne klikke på dots i slideshowert for at kunne skifte billede
+function currentSlide(n) {
+    slideIndex = n - 1;
+    showSlides();
+}
